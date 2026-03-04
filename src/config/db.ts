c3348aad2078ffcc0d712bfed2cee1384401971c
Khaled Hussein <khaledhussein957@gmail.com>
2026-03-04 15:36:19 +0300

    fix the issues

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,17 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI || "";
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+  throw new Error("MONGO_URI environment variable is not set");
+}
 
 const connectDB = async (): Promise<void> => {
   try {
     const conn = await mongoose.connect(MONGO_URI!);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log("Error connecting to database");
+    console.log("Error connecting to database", error);
     process.exit(1); // Exit process with failure 0 is success, 1 is failure
   }
 };
